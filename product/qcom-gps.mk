9e889b948c085b04f649217f6c685836c3edf9cb
Hriday Shamrma <hridaysharma42@gmail.com>
2017-07-04 10:31:36 +0000

    land: Move gps configs to tree
    Signed-off-by: Hriday Shamrma <hridaysharma42@gmail.com>

--- product/qcom-gps.mk
+++ product/qcom-gps.mk
@@ -6,6 +6,14 @@ PRODUCT_PACKAGES += \
     gps.default \
     libcurl
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/gps/etc/flp.conf:system/etc/flp.conf \
+    $(LOCAL_PATH)/gps/etc/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/gps/etc/izat.conf:system/etc/izat.conf \
+    $(LOCAL_PATH)/gps/etc/lowi.conf:system/etc/lowi.conf \
+    $(LOCAL_PATH)/gps/etc/sap.conf:system/etc/sap.conf \
+    $(LOCAL_PATH)/gps/etc/xtwifi.conf:system/etc/xtwifi.conf
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml
