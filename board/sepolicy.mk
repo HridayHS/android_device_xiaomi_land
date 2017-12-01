1e9ed63e3f4990b2f7c0965c01e7de5177f6fc1f
Vitaliy Tomin <highwaystar.ru@gmail.com>
2017-12-01 21:45:26 +0000

    land: Add seccomp policy

--- board/sepolicy.mk
+++ board/sepolicy.mk
@@ -1,3 +1,7 @@
 # SELinux
 #BOARD_SEPOLICY_DIRS += \
-#    $(DEVICE_PATH)/sepolicy
+    $(DEVICE_PATH)/sepolicy
+
+# Seccomp policy
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy
