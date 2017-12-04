416b37fb432b38c4aeaf8a3116813a28e306ca85
Hriday Sharma <hridaysharma42@gmail.com>
2017-12-04 09:07:32 +0000

    land: Remove fingerprint script
     * Reverts commit
     - https://github.com/HridayHS/android_device_xiaomi_land/commit/dd984fff8bd535260d4c826592df17c1c7ee5e8c#diff-7757ae3d5fee7079ec3e1c9d061f4d29
     - https://github.com/HridayHS/android_device_xiaomi_land/commit/7a183894e6253279b1ec6bbad94a4b6d8d5d2c6e#diff-7757ae3d5fee7079ec3e1c9d061f4d29

--- product/fingerprint.mk
+++ product/fingerprint.mk
@@ -3,9 +3,6 @@ PRODUCT_PACKAGES += \
     fingerprint.msm8937 \
     android.hardware.biometrics.fingerprint@2.0-service
 
-PRODUCT_PACKAGES += \
-    init.qcom.fingerprint.rc
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
