6e7d2449713d65ad9c3614c54d38f8dca64c501f
Hriday Sharma <hridaysharma42@gmail.com>
2017-12-04 09:18:38 +0000

    land: Add custom biometrics
     * Goodix hal uses old fingeprintd interface to send binder calls
     * Use new biometrics binder calls for fpc
    
    co-authored with:- @alexstsv2

--- product/fingerprint.mk
+++ product/fingerprint.mk
@@ -1,7 +1,7 @@
 # Fingerprint
 PRODUCT_PACKAGES += \
     fingerprint.msm8937 \
-    android.hardware.biometrics.fingerprint@2.0-service
+    android.hardware.biometrics.fingerprint@2.0-service-custom
 
 # Permissions
 PRODUCT_COPY_FILES += \
