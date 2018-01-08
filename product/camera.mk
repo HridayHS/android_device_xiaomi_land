d8750c6a4c92a61a66c9904b6d20c4694a9b6f7b
Hriday Sharma <hridaysharma42@gmail.com>
2018-01-08 09:13:57 +0000

    land: Add prebuilt libc
    Shimming executables are currently not working, so ship prebuilt
    libc for now to avoid camera being crashy.

--- product/camera.mk
+++ product/camera.mk
@@ -20,3 +20,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
+
+# Prebuilt libc
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/libshims/libc_32.so:system/lib/libc.so \
+    $(DEVICE_PATH)/libshims/libc_64.so:system/lib64/libc.so
