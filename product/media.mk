eafb54937f7dd87abae1f4ff1fcfc5114079b61d
Hriday Sharma <hridaysharma42@gmail.com>
2018-01-16 05:09:56 +0000

    land: Update seccomp policy from CAF
    - from LA.UM.6.6.r1-05300-89xx.0

--- product/media.mk
+++ product/media.mk
@@ -10,5 +10,5 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
-    $(DEVICE_PATH)/seccomp_policy/mediaextractor.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
+    $(DEVICE_PATH)/seccomp/mediacodec-seccomp.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
+    $(DEVICE_PATH)/seccomp/mediaextractor-seccomp.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
