4c6c3161f8fd89129bfe9ee7f80d59c69c8b4419
Hriday Sharma <hridaysharma42@gmail.com>
2018-01-17 12:46:05 +0000

    land: keylayout: Add keylayout for Redmi 3X

--- product/keylayout.mk
+++ product/keylayout.mk
@@ -1,6 +1,7 @@
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/keylayout/ft5x06_720p.kl:system/usr/keylayout/ft5x06_720p.kl \
+    $(DEVICE_PATH)/keylayout/ist30xx_ts_input.kl:system/usr/keylayout/ist30xx_ts_input.kl \
     $(DEVICE_PATH)/keylayout/gf3208.kl:system/usr/keylayout/gf3208.kl \
     $(DEVICE_PATH)/keylayout/msm8952-sku1-snd-card_Button_Jack.kl:system/usr/keylayout/msm8952-sku1-snd-card_Button_Jack.kl \
     $(DEVICE_PATH)/keylayout/uinput-fpc.kl:system/usr/keylayout/uinput-fpc.kl \
