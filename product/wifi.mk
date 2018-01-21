160a89af496e00459570d6250afafd86dafe1df0
Peter Qiu <zqiu@google.com>
2018-01-21 08:55:56 +0000

    land: Use hardware feature definition to indicate Passpoint support
    Bug: 36886243
    Test: Manual test
    Change-Id: I570f7037abfd670e153c62a8dc723f1e89e9bf5

--- product/wifi.mk
+++ product/wifi.mk
@@ -1,6 +1,7 @@
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:system/etc/permissions/android.hardware.wifi.passpoint.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml
 
 # Wi-Fi
