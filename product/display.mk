f383c28bebd86dc3e3a5ad2ec4e96070256815a5
Jesse Hall <jessehall@google.com>
2018-01-21 09:06:30 +0000

    land: Declare Vulkan compute level 0 system feature
    Bug: 38396207 (VK_KHR_variable_pointers)
    Bug: 64982680 (maxPerStageDescriptorStorageBuffers >= 16)
    Test: CtsGraphicsTestCases:.VulkanFeaturesTest (ag/2810565)
    Change-Id: I61cde61f53f0e9a1c657e6bd6658a4393360b0a6

--- product/display.mk
+++ product/display.mk
@@ -17,6 +17,7 @@ PRODUCT_COPY_FILES += \
 
 # Vulkan
 PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:system/etc/permissions/android.hardware.vulkan.compute.xml \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:system/etc/permissions/android.hardware.vulkan.level-0.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version-1_0_3.xml
 
